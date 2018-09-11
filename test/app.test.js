cbd3fd19c03fff6089fb3621ad344b27697a3ef8
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2018-09-11 20:55:49 -0300

    Implements update card test

--- test/app.test.js
+++ test/app.test.js
@@ -2,10 +2,16 @@ const request = require('supertest');
 const expect = require('chai').expect;
 const knex = require('../db/knex');
 const app = require('../app');
-const seed_cards = require('../support/seeds/cards');
+const seedCards = require('../support/seeds/cards');
+
+const testPut = {
+  title: 'João Pessoa - Paraíba',
+  description: 'City of the sun',
+  latitude: -7.115,
+  longitude: -34.86306,
+};
 
 describe('CRUD cards test', () => {
-
   before((done) => {
     knex.migrate.latest()
       .then(() => {
@@ -40,7 +46,20 @@ describe('CRUD cards test', () => {
   it('Should success if insert a speficic card', (done) => {
     request(app)
       .post('/api/cards/')
-      .send(seed_cards[0])
+      .send(seedCards[0])
+      .set('Accept', 'application/json')
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(200)
+      .then((response) => {
+        expect(response.body).to.be.a('object');
+        console.log(response.body);
+        done();
+      });
+  });
+  it('Should success if update a speficic card', (done) => {
+    request(app)
+      .put('/api/cards/1')
+      .send(testPut)
       .set('Accept', 'application/json')
       .expect('Content-Type', 'application/json; charset=utf-8')
       .expect(200)
