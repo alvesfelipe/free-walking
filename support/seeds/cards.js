ca8aba80a847a50211c1c202a91318c9f20de9b8
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2018-09-11 04:05:51 -0300

    Adds support files

---
+++ support/seeds/cards.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    title: 'João Pessoa',
+    description: 'City of the sun',
+    latitude: -7.115,
+    longitude: -34.86306,
+  },
+  {
+    title: 'Natal',
+    description: 'City of Rio Grande do Norte state',
+    latitude: -5.812757,
+    longitude: -35.255127,
+  },
+  {
+    title: 'Teresina',
+    description: 'City of Teresina state',
+    latitude: -5.08917,
+    longitude: -42.80194,
+  },
+];
