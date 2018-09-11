169c3999d658e0f37452dbf56662c50cda4f648b
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2018-09-11 04:13:50 -0300

    Updates app.js

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 const express = require('express');
+const http = require('http');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const config = require('./config/settings');
 const cards = require('./routes/card');
 
 const app = express();
 
+const server = http.createServer(app);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// allow cross origin requests
+app.use((req, res, next) => {
+  res.header('Content-Type', 'application/json');
+  next();
+});
+
 app.use('/api/cards', cards);
 
-app.listen('3000', () => {
+// Starting the server
+server.listen('3000', () => {
   console.log('Server running on http://%s:%s', config.host, config.port);
 });
+
+// Route not found (404)
+app.use((req, res) => res.status(404).send({ message: `Route, ${req.url} Not found.` }));
+
+// 500 - Any server error
+app.use((err, req, res) => res.status(500).send({ error: err }));
+
+module.exports = app;
