a5c4b42d3c72cc0bed1cedb8a2fec282e595d884
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2018-09-16 22:07:50 -0300

    Adds hashtags and cards_hashtags tables

--- db/migrations/20180910001332_create-card.js
+++ db/migrations/20180910001332_create-card.js
@@ -6,19 +6,19 @@ exports.up = (knex, Promise) => Promise.all([
     table.decimal('latitude', [8], [6]);
     table.decimal('longitude', [9], [6]);
   }),
+  knex.schema.createTable('hashtags', (table) => {
+    table.increments('id').primary();
+    table.text('hashtag');
+  }),
   knex.schema.createTable('cards_hashtags', (table) => {
     table.increments('id').primary();
     table.integer('cards_id').references('cards.id');
     table.integer('hashtags_id').references('hashtags.id');
   }),
-  knex.schema.createTable('hahstags', (table) => {
-    table.increments('id').primary();
-    table.text('hashtag');
-  }),
 ]);
 
 exports.down = (knex, Promise) => Promise.all([
-  knex.schema.dropTable('card'),
+  knex.schema.dropTable('cards'),
   knex.schema.dropTable('cards_hashtags'),
   knex.schema.dropTable('hashtags'),
 ]);
