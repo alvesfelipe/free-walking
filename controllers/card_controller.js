77e38658a8d1b0cbe697c925b2ec5179542568e6
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2018-09-16 22:06:37 -0300

    Implements insert in cards and hashtags

--- controllers/card_controller.js
+++ controllers/card_controller.js
@@ -1,4 +1,5 @@
-const queries = require('../db/queries');
+const queries = require('../db/queries/queries_cards');
+const queriesHashtags = require('../db/queries/queries_hashtags');
 
 exports.index = (req, res) => {
   queries.getAll().then((result) => {
@@ -13,9 +14,27 @@ exports.show = (req, res, next) => {
   });
 };
 
+const insertCardHashtag = (value, card) => queriesHashtags.getByValue(value).then((result) => {
+  if (!result) {
+    queriesHashtags.create({ hashtag: value }).then((hashtag) => {
+      queries.attachCardsHashtags(card.id, hashtag[0].id).then();
+    });
+  } else {
+    queries.attachCardsHashtags(card.id, result.id).then();
+  }
+});
+
 exports.insert = (req, res, next) => {
+  const hashtagsArray = req.body.hashtags;
+  delete req.body.hashtags;
   queries.create(req.body).then((card) => {
-    res.json(card[0]);
+    if (hashtagsArray) {
+      for (let i in hashtagsArray) { // eslint-disable-line
+        insertCardHashtag(hashtagsArray[i], card[0]);
+      }
+    }
+    const resCard = card[0] + { hashtags: hashtagsArray };
+    res.json(resCard);
   });
 };
 
